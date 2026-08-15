61eb93c9f91a4cc614ead02fc12bfb9ca255f13a
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-08-16 00:35:53 +0200

    Promote Control Center loader to rc1

--- src/sidebar-active.js
+++ src/sidebar-active.js
@@ -1,6 +1,6 @@
 (() => {
   const dashboardUrl = "/right.cgi";
-  const installedVersion = "5.0.0-beta1";
+  const installedVersion = "5.0.0-rc1";
   const releaseDate = "16-08-2026";
   const versionUrl = "https://raw.githubusercontent.com/PascalVZ96/MemoCraft-Theme/main/version.json";
   const i18nUrl = "/memocraft-theme/memo-i18n.js";
