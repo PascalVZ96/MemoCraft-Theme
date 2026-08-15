4eedf6039d81c022167c748935abb53a3e2b77b6
PascalVZ96 <pascal@pascalvanzalk.nl>
2026-08-16 00:29:54 +0200

    Load beta1 readiness runtime and bump sidebar version

--- src/sidebar-active.js
+++ src/sidebar-active.js
@@ -1,6 +1,6 @@
 (() => {
   const dashboardUrl = "/right.cgi";
-  const installedVersion = "5.0.0-alpha24";
+  const installedVersion = "5.0.0-beta1";
   const releaseDate = "16-08-2026";
   const versionUrl = "https://raw.githubusercontent.com/PascalVZ96/MemoCraft-Theme/main/version.json";
   const i18nUrl = "/memocraft-theme/memo-i18n.js";
@@ -229,6 +229,7 @@
         addRuntime(doc, 'data-memo-v5-reliability', '/memo-network/control-center-reliability.js');
         addRuntime(doc, 'data-memo-v5-healthscore', '/memo-network/control-center-healthscore.js');
         addRuntime(doc, 'data-memo-v5-intelligence', '/memo-network/control-center-intelligence.js');
+        addRuntime(doc, 'data-memo-v5-readiness', '/memo-network/control-center-readiness.js');
         addRuntime(doc, 'data-memo-v5-service-details', '/memo-network/control-center-services.js');
         addRuntime(doc, 'data-memo-v5-container-monitor', '/memo-network/control-center-container-monitor.js');
         addRuntime(doc, 'data-memo-v5-diagnostics', '/memo-network/control-center-diagnostics.js');
